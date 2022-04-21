91f6c9c879180e0056de3b514d0514a43de9a97d
Tamara Simikj <tamara.simic12@gmail.com>
2022-04-22 00:11:32 +0200

    Initial commit with security

---
+++ src/main/java/com/wp/project/beautysalon/service/SalonServiceService.java
@@ -0,0 +1,26 @@
+package com.wp.project.beautysalon.service;
+
+import com.wp.project.beautysalon.model.SalonService;
+import com.wp.project.beautysalon.model.User;
+
+import java.util.List;
+
+public interface SalonServiceService {
+    List<SalonService> findAll();
+
+    SalonService findbyId(String id);
+
+    List<SalonService> findAllById(List<String> servicesIds);
+
+    SalonService findbyserviceName(String serviceName);
+
+   // SalonService create(String id, String serviceName, Integer price, List<Long> employeeIds);
+
+    SalonService update(String id, String serviceName, Integer price , List<Long> employeeIds);
+
+    SalonService delete(String id);
+
+    void rate(SalonService usluga, Integer value, String comment, User client);
+
+
+}
